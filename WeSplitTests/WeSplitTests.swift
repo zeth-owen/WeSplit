552bb0d28105b1fc175bed5eff868d1bcebfca73
Zeth Thomas <zeththomas89@gmail.com>
2024-11-10 08:23:55 -0800

    Initial Commit

---
+++ WeSplitTests/WeSplitTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitTests.swift
+//  WeSplitTests
+//
+//  Created by Zeth Thomas on 11/10/24.
+//
+
+import Testing
+@testable import WeSplit
+
+struct WeSplitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
